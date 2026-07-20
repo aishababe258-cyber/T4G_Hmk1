59f641b9067260ab9599aaf995291e4a0e4ea08c
aishababe258-cyber <aishababe258@gmail.com>
2026-07-20 06:11:44 -0700

    modify all files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Taking input from the user and storing it in a variable called  userName.
+let userName = prompt("Enter your name");
+// Prompt the user for their details
+let age = prompt("Enter your age:");
+let email = prompt("Enter your email:");
+
+// Display the information
+alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
