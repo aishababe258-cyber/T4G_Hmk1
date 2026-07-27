ec89b1a43bb9c49a707b4ccfc5af9ba148670d2b
aishababe258-cyber <aishababe258@gmail.com>
2026-07-27 08:40:39 -0700

    Modified src/index.js file

--- src/index.js
+++ src/index.js
@@ -5,4 +5,44 @@ let age = prompt("Enter your age:");
 let email = prompt("Enter your email:");
 
 // Display the information
-alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
+alert(`Hi ${userName}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
+
+// A. Store your name inside a variable and log it to the console.
+let myName = "Aisha Abdul Razak";
+console.log(myName);
+
+// B. Store your age inside a variable and log it to the console.
+let myAge = 22;
+console.log(myAge);
+
+// C. Add two numbers.
+let addition = 10 + 5;
+console.log(addition);
+
+// D. Subtract two numbers.
+let subtraction = 10 - 5;
+console.log(subtraction);
+
+// E. Divide two numbers.
+let division = 10 / 5;
+console.log(division);
+
+// F. Multiply two numbers.
+let multiplication = 10 * 5;
+console.log(multiplication);
+
+// G. Find the remainder of 15 % 4.
+let remainder = 15 % 4;
+console.log(remainder);
+
+// H. Store a boolean value.
+let isStudent = true;
+console.log(isStudent);
+
+// I. Store a number and log its type.
+let myNumber = 100;
+console.log(typeof myNumber);
+
+// J. Store a string and log its type.
+let myString = "Tech4Girls";
+console.log(typeof myString);
